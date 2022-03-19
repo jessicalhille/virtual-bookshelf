c44e0671afdf31cb90f073864a03e20e4467cf70
Jessica Hille <jessicalhille@gmail.com>
2022-03-19 13:56:53 -0500

    initial commit, back end complete

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,41 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+    type Book {
+        bookId: String!
+        authors: [String]
+        description: String
+        title: String!
+        image: String
+        link: String
+    }
+
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: SavedBookInput): User
+        removeBook(bookId: String!): User
+    }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
